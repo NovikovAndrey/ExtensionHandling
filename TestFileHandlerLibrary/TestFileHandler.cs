478c78e1901c7a44a6ce1a0b72f809e4d08ddb5e
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-01-31 23:30:37 +0300

    Write dll

--- TestFileHandlerLibrary/TestFileHandler.cs
+++ TestFileHandlerLibrary/TestFileHandler.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Web;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace TestFileHandlerLibrary
 {
-    public class TestFileHandler
+    public class TestFileHandler : IHttpHandler
     {
+        public bool IsReusable => false;
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string filePath = context.Request.PhysicalPath;
+            using (System.IO.FileStream stream = new System.IO.FileStream(filePath, System.IO.FileMode.Open))
+            {
+                System.Xml.Linq.XDocument xDocument = System.Xml.Linq.XDocument.Load(stream);
+                var allUsers = from user in xDocument.Descendants("User")
+                               select new
+                               {
+                                   FirstName = user.Element("FirstName").Value,
+                                   LastName = user.Element("LastName").Value
+                               };
+                context.Response.Write("<html><body><table border='1'>");
+                foreach (var user in allUsers)
+                {
+                    context.Response.Write("<tr>");
+                    context.Response.Write("<td>"+user.FirstName+"</td>");
+                    context.Response.Write("<td>"+user.LastName+"</td>");
+                    context.Response.Write("</tr>");
+                }
+                context.Response.Write("</table></body></html>");
+            }
+        }
     }
 }
